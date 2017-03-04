e1f5bb4d4e87e8433eb6393d9979c830615dc331
flor condori <condorihuamanif@gmail.com>
2017-03-04 12:12:48 -0500

    modificando Expresiones regulares

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,25 @@
+//obtener el botn y el Div por el Id
 var imprimir = document.getElementById("imprimir");
 var formulario = document.getElementById("formulario");
-
+//cuando haga click al boton
 imprimir.onclick = function (){
 	var nombre = document.getElementById("nombre").value;
 	var apellido = document.getElementById("apellido").value;
 	var dni = document.getElementById("dni").value;
 	var direccion = document.getElementById("direccion").value;
-
+//validar que los campos esten llenos para recien imprimir
 	if(nombre!="" && apellido!="" && dni!="" && direccion!=""){
-		if(dni.length==8){
+		if(dni.length==8){//validar DNI
 			formulario.innerHTML = "<ul>"+
-									"<li> Nombre: "+nombre+"</li>"+
-						 			"<li> Apellido: "+apellido+"</li>"+
-						 			"<li> DNI: "+dni+"</li>"+
-						 			"<li> Direccion: "+direccion+"</li>"+
+									"<li><strong>Nombre: </strong>"+nombre+"</li>"+
+						 			"<li><strong>Apellido: </strong>"+apellido+"</li>"+
+						 			"<li><strong>DNI: </strong>"+dni+"</li>"+
+						 			"<li><strong>Direccion: </strong>"+direccion+"</li>"+
 						 		"</ul>";
-		
-		}else{
-			alert("campo DNI debe contener solo 8 digitos ")
-		}
 
-		
+		}else{
+					alert("campo DNI debe contener solo 8 digitos ")
+				}
 	}
 
 }
